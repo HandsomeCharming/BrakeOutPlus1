f44ea7736461031e82c77185d5f32a56f34a181b
Handa Zhang <handazh2@gmail.com>
2018-05-26 19:11:54 -0700

    color change

--- Assets/Scripts/UI/TabUIColor.cs
+++ Assets/Scripts/UI/TabUIColor.cs
@@ -9,14 +9,20 @@ public class TabUIColor : MonoBehaviour {
     public Image[] images;
     public Text[] texts;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    bool inPlayTab = true;
+    Color otherColor;
+    Color skyColor;
+
+    // Use this for initialization
+    void Awake () {
+        inPlayTab = true;
+        otherColor = new Color(37.0f/255.0f, 170.0f / 255.0f, 225.0f / 255.0f);
+        skyColor = new Color(37.0f / 255.0f, 170.0f / 255.0f, 225.0f / 255.0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if(BackgroundManager.current.m_Background == BackgroundEnum.Color)
+		if(BackgroundManager.current.m_Background == BackgroundEnum.Color && inPlayTab == true)
         {
             Color col = background.GetCurrentDownColor();
 
@@ -32,20 +38,38 @@ public class TabUIColor : MonoBehaviour {
         }
 	}
 
+    public void SetColorInPlayTab()
+    {
+        inPlayTab = true;
+    }
+
+    public void SetColorInOtherTabs()
+    {
+        inPlayTab = false;
+
+        foreach (var image in images)
+        {
+            image.color = otherColor;
+        }
+
+        foreach (var text in texts)
+        {
+            text.color = otherColor;
+        }
+    }
+
     public void ChangeBackground()
     {
         if(BackgroundManager.current.m_Background == BackgroundEnum.SkyCity)
         {
-            Color col = Color.blue;
-
             foreach (var image in images)
             {
-                image.color = col;
+                image.color = skyColor;
             }
 
             foreach (var text in texts)
             {
-                text.color = col;
+                text.color = skyColor;
             }
         }
     }
