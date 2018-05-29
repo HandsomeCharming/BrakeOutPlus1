b6569fb06aa0d6870f11e52370d62fff3391a0ae
Handa Zhang <handazh2@gmail.com>
2018-05-28 17:12:44 -0700

    filling trello list

---
+++ Assets/Scripts/UI/ButtonSoundOnClick.cs
@@ -0,0 +1,27 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonSoundOnClick : MonoBehaviour {
+
+    public enum SoundType
+    {
+        Button,
+        Tab
+    }
+    public SoundType soundType = SoundType.Button;
+
+	// Use this for initialization
+	void Start () {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(PlayUISound);
+	}
+	
+    void PlayUISound()
+    {
+        string eventName = soundType == SoundType.Button ? "click" : "click2";
+        AudioSystem.current.PlayEvent(eventName);
+    }
+}
