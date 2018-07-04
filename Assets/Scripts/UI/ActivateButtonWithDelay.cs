8b874bc8123ee339a807a7c37a573d3f61369b2a
Handa Zhang <handazh2@gmail.com>
2018-07-04 16:37:13 -0700

    added tutorial delay

---
+++ Assets/Scripts/UI/ActivateButtonWithDelay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ActivateButtonWithDelay : MonoBehaviour {
+    public float enableTime = 1.0f;
+
+    private void OnEnable()
+    {
+        GetComponent<Button>().enabled = false;
+        Invoke("EnableButton", enableTime);
+    }
+
+    void EnableButton()
+    {
+        GetComponent<Button>().enabled = true;
+    }
+}
