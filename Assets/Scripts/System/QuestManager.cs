8d3d9066eba1f9011719a20888a48d737755da3b
Handa Zhang <handazh2@gmail.com>
2018-07-04 21:40:22 -0700

    quest fix

--- Assets/Scripts/System/QuestManager.cs
+++ Assets/Scripts/System/QuestManager.cs
@@ -217,7 +217,6 @@ public class QuestManager : MonoBehaviour {
 
     bool ShouldGetDailyQuest()
     {
-        return true;
         if(RecordManager.HasRecordDate(LastDailyDateKey))
         {
             DateTime lastDailyDate = RecordManager.GetRecordDate(LastDailyDateKey);
