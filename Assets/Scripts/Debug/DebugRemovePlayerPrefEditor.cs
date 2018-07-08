82d9250742a8cdc7f5d67ac5a6281a08b582fc8a
Handa Zhang <handazh2@gmail.com>
2018-07-08 13:54:06 -0700

    fixed debug editor

--- Assets/Scripts/Debug/DebugRemovePlayerPrefEditor.cs
+++ Assets/Scripts/Debug/DebugRemovePlayerPrefEditor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(DebugFunctionCaller))]
+[CustomEditor(typeof(DebugRemovePlayerPrefEditor))]
 public class DebugRemovePlayerPrefEditor : Editor {
 
     public override void OnInspectorGUI()
